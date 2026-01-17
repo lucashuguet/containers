15a529953a1bda79899b1f319ea7db4cc8612a6a
Lucas Huguet <lucashuguet31@gmail.com>
2026-01-18 00:40:12 +0100

    some changes on jupyter

--- jupyter/open.sh
+++ jupyter/open.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-firefox --private-window http://localhost:4005
+token=$(docker logs jupyter 2>&1 | grep -o 'token=[a-f0-9]*' | tail -1)
+firefox --private-window http://localhost:4005/lab?$token
